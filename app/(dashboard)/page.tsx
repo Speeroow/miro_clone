9a775eb8f7d746eaaf3339fffbbaaaaaf4c38b37
Speeroow <abhishek.rajawat1909@gmail.com>
2024-02-27 07:59:45 +0530

    first commit

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+  return (
+    <main>
+      <div>Dashboard Root Page</div>
+    </main>
+  );
+};
+
+export default DashboardPage;
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://logical-dove-99.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
