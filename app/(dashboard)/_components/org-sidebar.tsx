33047fe30a36ed59ebde0ab93c1e98783c8a8814
Speeroow <abhishek.rajawat1909@gmail.com>
2024-02-28 20:35:40 +0530

    pending

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,61 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+import { Poppins } from "next/font/google";
+import { OrganizationSwitcher } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { LayoutDashboard } from "lucide-react";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org Sidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image alt="Logo" src="/logo.svg" height={60} width={60} />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+            },
+          },
+        }}
+      />
+      <div className="space-y-1 w-full">
+        <Button
+          asChild
+          size='lg'
+          className="font-normal justify-start px-2 w-full"
+        >
+          <Link href="/">
+            <LayoutDashboard className="h-4 w-4 mr-2" />
+            Team boards
+          </Link>
+        </Button>
+      </div>
     </div>
   );
 };
