0100ea91fc59d8db7d4ad0f8086a452dad6122a9
Speeroow <abhishek.rajawat1909@gmail.com>
2024-03-20 22:05:21 +0530

    room auth completed

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,13 +4,18 @@ import { Info } from "./info"
 import { Participants } from "./participants"
 import { Toolbar } from "./toolbar"
 
+import { useSelf } from "@/liveblocks.config"
+
 interface CanvasProps{
     boardId:string;
 }
 
 export const Canvas = ({
     boardId
-}:CanvasProps) => {
+}: CanvasProps) => {
+    const info = useSelf((me) => me.info)
+    console.log(info)
+
     return (
         <main className="h-full w-full relative bg-neutral-100 touch-none">
             <Info />
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -2,6 +2,8 @@ import { Canvas } from "./_components/canvas";
 import { Room } from "@/components/room";
 import { Loading } from "./_components/loading";
 
+
+
 interface BoardIdPageProps {
   params: {
     boardId: string;
